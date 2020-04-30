700ab48564b2c7d1c7f0da51bb795d83f6e78367
Denis Protopopov <protdenden@gmail.com>
2020-04-30 22:40:47 +0300

    Portal added

--- GesturesAR/SecondViewController.swift
+++ GesturesAR/SecondViewController.swift
@@ -19,7 +19,7 @@ class SecondViewController: UIViewController {
         
         private var serialQueue = DispatchQueue(label: "dispatchqueueml")
         private var visionRequests = [VNRequest]()
-        let object = SCNScene(named: "word.scn")!.rootNode.clone()
+        let object = SCNScene(named: "art.scnassets/portal.scn")!.rootNode.clone()
     }
         
     // MARK: - Lifecycle
@@ -50,8 +50,6 @@ class SecondViewController: UIViewController {
               //  self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,
               //  ARSCNDebugOptions.showFeaturePoints]
             setupAR()
-            setupML()
-            loopCoreMLUpdate()
             setUpCoachingOverlay()
                 
             }
@@ -81,15 +79,7 @@ class SecondViewController: UIViewController {
             sceneView.session.run(configuration)
         }
         
-        private func setupML() {
-            guard let selectedModel = try? VNCoreMLModel(for: example_5s0_hand_model().model) else {
-                    fatalError("Could not load model.")
-            }
-            
-            let classificationRequest = VNCoreMLRequest(model: selectedModel, completionHandler: classificationCompleteHandler)
-            visionRequests = [classificationRequest]
-            
-        }
+        
     }
     // MARK: - Private
     extension SecondViewController {
@@ -105,15 +95,17 @@ class SecondViewController: UIViewController {
                 
             } else {
                 
+               // SCNVector3(x: hitResult.worldTransform.columns.3.x, y: //hitResult.worldTransform.columns.3.y + 0.05, z: hitResult.worldTransform.columns.3.z)
+                
                 let columns = hitTest.first?.worldTransform.columns.3
-                object.position = SCNVector3(x: columns!.x, y: columns!.y+0.2, z: columns!.z)
-                let min = object.boundingBox.min
-                let max = object.boundingBox.max
-                object.pivot = SCNMatrix4MakeTranslation(
-                    min.x + (max.x - min.x)/2,
-                    min.y + (max.y - min.y)/2,
-                    0
-                )
+                object.position = SCNVector3(x: columns!.x, y: columns!.y, z: columns!.z-1)
+               // let min = object.boundingBox.min
+                //let max = object.boundingBox.max
+               // object.pivot = SCNMatrix4MakeTranslation(
+                  // min.x + (max.x - min.x)/2,
+              //      min.y + (max.y - min.y)/2,
+              //     min.z + (max.z - min.z)/2
+          //      )
                 
                 self.sceneView.scene.rootNode.addChildNode(object)
                 sceneView.autoenablesDefaultLighting = true
@@ -122,75 +114,7 @@ class SecondViewController: UIViewController {
             
         }
         
-        private func loopCoreMLUpdate() {
-            if !runloopCoreMLUpdate { return }
-            serialQueue.async {
-                self.updateCoreML()
-                self.loopCoreMLUpdate()
-                
-            }
-            
-        }
+     
         
     }
-    // MARK: - Private
-    extension SecondViewController {
-        private func updateCoreML() {
-            if !runupdateCoreML { return }
-            let pixbuff : CVPixelBuffer? = (sceneView.session.currentFrame?.capturedImage)
-            if pixbuff == nil { return }
-            let ciImage = CIImage(cvPixelBuffer: pixbuff!)
-            let imageRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
-            do {
-                try imageRequestHandler.perform(self.visionRequests)
-            } catch {
-                print(error)
-            }
-        }
-    }
-    // MARK: - Private
-    extension SecondViewController {
-        private func classificationCompleteHandler(request: VNRequest, error: Error?) {
-            if error != nil {
-                print("Error: " + (error?.localizedDescription)!)
-                return
-                
-            }
-            
-            guard let observations = request.results else {
-                print("No results")
-                return
-                
-            }
-            
-            let classifications = observations[0...2]
-                .compactMap({ $0 as? VNClassificationObservation })
-                .map({ "\($0.identifier) \(String(format:" : %.2f", $0.confidence))" })
-                .joined(separator: "\n")
-            
-            DispatchQueue.main.async {
-                let topPrediction = classifications.components(separatedBy: "\n")[0]
-                let topPredictionName = topPrediction.components(separatedBy: ":")[0].trimmingCharacters(in: .whitespaces)
-                let topPredictionScore:Float? = Float(topPrediction.components(separatedBy: ":")[1].trimmingCharacters(in: .whitespaces))
-                
-                if (topPredictionScore != nil && topPredictionScore! > 0.10) {
-                    if topPredictionName == "FIVE-UB-RHand" {
-                        
-                        self.object.runAction(SCNAction.rotateBy(x: 0.0, y: 0.0, z: 1.0, duration: 200.0))
-                        print("FIVE")
-                        
-                    }
-                    
-                    if topPredictionName == "fist-UB-RHand" {
-                        self.object.removeAllActions()
-                        print("FIST")
-                        
-                    }
-                    
-                }
-                
-            }
-            
-        }
-        
-    }
+
